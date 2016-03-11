6edf4a7177524eafe2b9d3331471522198a01305
Adam Wilbert <adam.wilbert@gmail.com>
2016-03-11 13:00:15 -0800

    separating prompts container and stateless function, adding confirm battle structure;

---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,13 @@
+var React = require('react')
+var ConfirmBattle = require('../components/ConfirmBattle')
+
+far ConfirmBattleContainer = React.createClass({
+  render: function () {
+    return (
+      <ConfirmBattle />
+    )
+  }
+})
+
+
+module.exports = ConfirmBattleContainer
