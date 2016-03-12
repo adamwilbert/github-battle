1dc2e4007e35e6f2ee23a460f555a9065fd0db14
Adam Wilbert <adam.wilbert@gmail.com>
2016-03-12 08:35:54 -0800

    ui for winning/losing

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,5 +1,8 @@
 var React = require('react')
 var PropTypes = React.PropTypes
+var styles = require('../styles')
+var UserDetails = require('./UserDetails')
+var UserDetailsWrapper = require('./UserDetailsWrapper')
 
 
 function puke(object) {
@@ -7,8 +10,27 @@ function puke(object) {
 }
 
 function Results (props) {
+  var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1
+  var losingIndex = winningIndex === 0 ? 1 : 0
   return (
-    <div> Results {puke(props)}</div>
+    <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+      <h1>Results</h1>
+      <div className="col-sm-8 col-sm-offset-2">
+        <UserDetailsWrapper header="Winner">
+          <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]} />
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header="Loser">
+          <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]} />
+        </UserDetailsWrapper>
+      </div>
+
+      <div className='col-sm-12' style={styles.space}>
+        <Link to='/playerOne'>
+          <button type='button' className='btn btn-lg btn-danger'> Start Over </button>
+        </Link>
+      </div>
+
+    </div>
   )
 }
 
